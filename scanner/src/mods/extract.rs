b8d26677e79fccc50ad4b3016fc874bfeff76a0e
Siddhi <ssc67916@uga.edu>
2024-09-29 21:30:41 -0400

    finished TODO: Loop to try again after error or after completing the execution

--- scanner/src/mods/extract.rs
+++ scanner/src/mods/extract.rs
@@ -11,13 +11,22 @@ pub fn extract_file() -> [u8; 64]{
 
     // Reading the first 64 bytes of the file as bytes
     let result = file.read_exact(&mut file_content);
-
+    loop {
+        if result.is_err() {
+            println!("error - trying again!");
+            continue; 
+        } //if
+        break;
+    } //loop
+    
+    
+    
     // Quick exit if there was an error
     // TODO: Loop to try again after error or after completing the execution 
-    if result.is_err() {
-        println!("There was an error in the execution");
-        return [0; 64]
-    }
+   //if result.is_err() {
+       // println!("There was an error in the execution");
+       //return [0; 64]
+    //}
 
     // Printing the contents of the file. Only for debugging
     println!("\nThe contents of the file:");
@@ -28,6 +37,6 @@ pub fn extract_file() -> [u8; 64]{
     println!("\n2. Extracting or decoding the contents of the input file:");
 
     // TODO: Actually check here if we need to convert from srec/intel_hex/zip/gz/etc... to bin, convert it and return the first 64/128 bytes
-
+    
     file_content
 }
