d8e7cb27bb19d4f01040eda714dba238175a9f9d
Siddhi <ssc67916@uga.edu>
2024-09-30 18:24:00 -0400

    fixed the print.rs to have placeholder

--- scanner/src/mods/print.rs
+++ scanner/src/mods/print.rs
@@ -1,11 +1,4 @@
-use mods::extract::extract_file;
-use mods::verify::{verify_file, Signature};
-use mods::print::{print_firmware_contents, print_signature_data};
-
-fn main() {
-    let firmware_content = extract_file();
-    print_firmware_contents(&firmware_content);
-
-    let signatures: Vec<Signature> = verify_file(firmware_content);
-    print_signature_data(&signatures);
+//placeholder for now
+pub fn print_metadata() {
+    println!("Extracting and printing metadata!");
 }
