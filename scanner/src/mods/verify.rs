1a50ba03f06c762b4847a3afa12e3f8cc8f2fecd
Nicolas Nino <nfn57152@uga.edu>
2024-10-22 13:58:16 -0400

    Work towards reading the signature file and storing the information in the structures for matching

--- scanner/src/mods/verify.rs
+++ scanner/src/mods/verify.rs
@@ -9,6 +9,7 @@ use std::{fs::File, io::{BufRead, BufReader, Lines, Result}, path::Path};
 extern crate crc;
 const X25: crc::Crc<u16> = crc::Crc::<u16>::new(&crc::CRC_16_IBM_SDLC);
 
+use regex::Regex;
 
 #[derive(Debug)]
 pub struct Signature{
@@ -16,6 +17,7 @@ pub struct Signature{
 }
 
 #[derive(Debug)]
+#[derive(Clone)]
 struct Field {
     position: u32,
     value_type: FieldType,
@@ -24,20 +26,32 @@ struct Field {
 }
 
 #[derive(Debug)]
+#[derive(Clone)]
 enum FieldType {
-    Str(String),
-    Date(String),
-    Regex(String),
-    Byte(u8),
-    Short(u8, u8),
-    Long(u8, u8, u8, u8),
+    Str,
+    Date,
+    Regex,
+    Byte,
+    Short,
+    Long,
+    Default
 }
+
+impl Default for Field {
+    fn default() -> Self {
+        Field {
+            position: 0,
+            value_type: FieldType::Default,
+            constraint: String::from("Unknown"),
+            description: String::from("Unknown"),
+        }
+    }
+}
+
 
 pub fn verify_file(contents: Vec<u8>) -> Vec<Signature>{
     println!("\n3. Verifing the contents of the input file:");
-    let mut signatures = read_signatures();
-
-    let mut current_signature: Option<Signature> = None;
+    let signatures = read_signatures();
 
     let calc_crc = calculate_crc(&contents);
     let expect_crc: u16 = 0x906e;
@@ -49,25 +63,92 @@ pub fn verify_file(contents: Vec<u8>) -> Vec<Signature>{
         //ToD: handle this
     }
 
-    current_signature = Some(Signature {
-        fields: Vec::new()
-    });
-
-    signatures.push(current_signature.take().unwrap());
-
     signatures
 }
 
 fn read_signatures() -> Vec<Signature> {
     let mut results = Vec::new();
 
+    let re = Regex::new(r"\s+").unwrap();
+
     if let Ok(lines) = read_lines("magic/vendors") {
+        let mut fields_to_add: Vec<Field> = Vec::new();
         for line in lines.flatten() {
-            if line.as_str().starts_with("#") || line.as_str().starts_with(" ") {
-                // Ignore the lines that start with # or space
+
+            let mut field = Field {
+                ..Default::default()
+            };
+
+            // if line.as_str().starts_with("#") || line.as_str().starts_with("\n") || line.as_str().starts_with(" ") {
+            if line.as_str().starts_with("#") || line.len() == 0 {
+                // Ignore the lines that start with #
+                if line.as_str().starts_with(" ") || line.as_str().starts_with("\n") || line.len() == 0 {
+                    println!("EOS LINE:\n{}", line);
+                    if fields_to_add.len() > 0 {
+                        let current_signature = Signature {
+                            fields: fields_to_add.clone()
+                        };
+                        println!("{:#?}", current_signature);
+                        
+                        results.push(current_signature);
+                        fields_to_add.clear();
+                    }
+
+                } else {
+                    println!("ELSE: {}", line)
+                }
+                
             } else {
                 // Here is where the signature population should happen
-                println!("{}", line)
+                println!("GOOD LINE:\n{}", line);
+                //println!("Line:\n{:#?}", line.as_bytes());
+
+                let split = re.splitn(&line, 4);
+
+                for (index, item) in split.enumerate() {
+                    //println!("\n==========\nITEM: {:#?}\n==========\n", item.as_bytes());
+                    println!("\n==========\nITEM: {}\n==========\n", item);
+                    match index {
+                        0 => {
+                            let mut num: u32  = 0;
+                            if item.starts_with(">") {
+                                let re = Regex::new(r">\s*(\d+)").unwrap();
+                                if let Some(captures) = re.captures(item.trim()) {
+                                    // Extract the number
+                                    let number = &captures[1];
+                                    println!("Matched number: {}", number);
+                                    num = number.trim().parse().expect("Not a valid number!");
+                                    
+                                } else {
+                                    println!("No match found");
+                                }
+                                
+                            } else {
+                                num = item.trim().parse().expect("Not a valid number!");
+                            }
+                            field.position = num;
+                            
+                        }, 
+                        1 => {
+                            match item {
+                                "byte" => { field.value_type = FieldType::Byte; },
+                                "short" => { field.value_type = FieldType::Short; },
+                                "long" => { field.value_type = FieldType::Long; },
+                                "string" => { field.value_type = FieldType::Str; },
+                                "Date" => { field.value_type = FieldType::Date; },
+                                "Regex" => { field.value_type = FieldType::Regex; },
+                                &_ => { field.value_type = FieldType::Default; },
+                            }
+                        }, 
+                        2 => {
+                            field.constraint = item.to_string();
+                        },
+                        _ => {
+                            field.description = item.to_string();
+                        }
+                    }
+                }
+                fields_to_add.push(field);
             }
         }
     }
